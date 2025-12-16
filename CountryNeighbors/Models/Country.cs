d8fae3d11e80e4f08942ad68c122eda4f0d60dcd
Garima Madaan <135014389+GarimaMadaan@users.noreply.github.com>
2025-12-16 23:41:49 +0530

    Neighboring Countries code added

---
+++ CountryNeighbors/Models/Country.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CountryNeighborLookup.Models
+{
+    public class Country
+    {
+        public string Code { get; set; } = string.Empty;
+        public List<string> NeighboringCountries { get; set; } = new();
+    }
+}
