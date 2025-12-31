cbd1e6a1c92ef4037ee8365c1be0a0a0ea743610
GarimaMadaanITT <garima.madaan@intimetec.com>
2025-12-31 09:46:01 +0530

    Book Management System Code Added

---
+++ BookManagementSystem/Models/BookLocation.cs
@@ -0,0 +1,19 @@
+﻿namespace BookManagementSystem.Models
+{
+    public class BookLocation
+    {
+        public string Room { get; }
+        public string Shelf { get; }
+
+        public BookLocation(string room, string shelf)
+        {
+            Room = room;
+            Shelf = shelf;
+        }
+
+        public override string ToString()
+        {
+            return $"Room: {Room}, Shelf: {Shelf}";
+        }
+    }
+}
---
+++ BookManagementSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BookManagementSystem.Models;
+using BookManagementSystem.Services;
+
+class Program
+{
+    static void Main()
+    {
+        var location = new BookLocation("Reading Room", "Shelf 5");
+        var book = new Book("A Great Book", "John Doe", location);
+
+        var printer = new HtmlPagePrinter();
+        var repository = new FileBookRepository();
+
+        printer.PrintPage(book.GetCurrentPage());
+        repository.Save(book);
+
+        Console.WriteLine($"Book Location: {book.Location}");
+    }
+}
